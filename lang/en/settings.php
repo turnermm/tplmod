1ecf700e589f75bf941cee56aba4cdfda227aa9c
Myron Turner <turnermm02@shaw.ca>
2017-07-04 09:36:40 -0500

    added lang

---
+++ lang/en/settings.php
@@ -0,0 +1,15 @@
+<?php
+$lang['logos'] = 'Comma separated list of image names for home page logo: names only, not paths';
+$lang['ips'] = 'Comma separated list of IPs for rotation by IP; rotations follow sequence of IPs';
+$lang['sitetools'] = '<b>Site Tools</b><br />Remove the selected site tools. In the Dokuwiki template Site Tools are located at the top of the template.';
+$lang['pagetools'] = '<b>Page Tools</b><br />Remove the selected page tools. In the Dokuwiki template these are located along the right-hand side of the template. ' .
+' You can remove non-standard tools, using the input box:  enter them as a comma separated list of unique words from their query strings or urls.';
+$lang['profile'] = 'Remove User Profile link';
+$lang['search'] = 'Remove Search box';
+$lang['taglines'] = "Comma separated list of taglines";
+$lang['acl_all'] = 'The acl level at which sitetoools and pagetools removals take effect.';
+$lang['dateorip'] = 'Rotate logos, tag lines, and wiki names  by ip address or days of the week. Select  <code>NEITHER</code> if you do not want rotation.';
+$lang['rotatewhich'] = "If rotating the logo and/or tag line, select which to rotate";
+$lang['ptools_xcl'] = ' If pagetools is set to <code>All</code>, you can retain selected tools by entering  a comma separated list of unique words from their urls in this text box';
+$lang['wiki_names'] = "Comma separated list of wiki names";
+$lang['rotate_title'] = "Rotate the wiki name (title)";
