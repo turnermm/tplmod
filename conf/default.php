49c9243c0114fc1fbf3c084d48a58121b7c28064
Myron Turner <turnermm02@shaw.ca>
2017-07-04 09:35:14 -0500

    added config

---
+++ conf/default.php
@@ -0,0 +1,14 @@
+<?php
+$conf['sitetools'] = '';
+$conf['pagetools'] = '';
+$conf['profile'] = 0;
+$conf['search'] = 0;
+$conf['acl_all'] ='READ';
+$conf['logos'] = "";
+$conf['ips']  = "";
+$conf['taglines']  = "";
+$conf['dateorip'] = 'NEITHER';
+$conf['rotatewhich'] ='NONE';
+$conf['ptools_xcl'] =  'dokuwiki__top';
+$conf['wiki_names'] = "";
+$conf['rotate_title'] = 0;
---
+++ conf/metadata.php
@@ -0,0 +1,16 @@
+<?php
+$meta['sitetools']  = array('multicheckbox','_choices' => array('Recent Changes','Media Manager','Sitemap'));
+$meta['pagetools']  = array('multicheckbox','_choices' => array('Edit','Old Revisions','Backlinks', 'Subscribe','All'));
+$meta['ptools_xcl'] = array('string');
+$meta['profile'] = array('onoff');
+$meta['search'] = array('onoff');
+$meta['taglines']  = array('string');
+$meta['logos'] = array('string');
+$meta['ips']  = array('string');
+$meta['rotate_title'] = array('onoff');
+$meta['wiki_names'] = array('string');
+$meta['acl_all'] = array('multichoice','_choices'=>array('NONE','READ','EDIT','CREATE','UPLOAD'));
+$meta['dateorip'] =  array('multichoice','_choices' => array('NEITHER','IP','DAY'));
+$meta['rotatewhich'] =  array('multichoice','_choices' => array('NONE','LOGO','TAG','BOTH'));
+
+
